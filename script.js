db8d03beefe7288d4a4ef1b2a040c242c8ded87b
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-06-02 02:41:42 +0300

    pro

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+
+
+
+
+
+
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
