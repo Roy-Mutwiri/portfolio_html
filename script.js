d804572bcc0856d68bc7d2e07e1dc5a159936c16
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-06-02 02:19:40 +0300

    stticky navbar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec =>{
@@ -8,13 +8,18 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top <offset + height){
+        if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav')
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
 
-            })
+
+            });
 
         };
-    })
-}
+    });
+
+    /*-------------Sticky navbar---------------*/
+
+    
+};
