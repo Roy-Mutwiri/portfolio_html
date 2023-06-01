1fff4ce7c6a825266a57a1605bfe89bda60642dc
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-06-02 02:01:22 +0300

    js

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('section');
 
 window.onscroll = () => {
-    
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top <offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav')
+
+            })
+
+        };
+    })
 }
