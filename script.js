185f1a11ae1f02decdb61d3b0ccc1607ce832711
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-06-02 03:35:42 +0300

    weuh

--- script.js
+++ script.js
@@ -52,3 +52,29 @@ window.onscroll = () => {
 
 
 };
+
+
+
+
+
+
+
+
+/*-----------Sr=crolll---------------*/
+
+ScrollReveal({  
+    //reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay: 200
+
+});
+
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+
+ScrollReveal().reveal('.home-content h1, .about-img',{ origin: 'left' });
+
+ScrollReveal().reveal('.home-content p, .about-content',{ origin: 'right' });
