77980b0abb013ff590da9baa54b44d19c241d86b
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-06-02 16:56:04 +0300

    editing

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ ScrollReveal().reveal('.home-content p, .about-content',{ origin: 'right' });
 
 const typed = new Typed('.multiple-text',{
 
-    strings: ['Python Developer','Web Developer','Database Administrator'],
+    strings: ['Pyth0n Developer,','Web Developer,','Database Administrator.'],
     typeSpeed:100,
     backspeed: 100,
     backDelay:1000,
